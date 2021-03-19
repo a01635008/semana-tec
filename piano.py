6cc4ffaacd3c1774b037b66ff29ff73eec970dd8
Sebastián González Cabrera <a01635008@itesm.mx>
2021-03-19 12:03:54 -0600

    pep8 3

--- piano.py
+++ piano.py
@@ -14,7 +14,6 @@ pygame.mixer.music.set_volume(0.8)
 
 class PianoKey:
 
-
     def __init__(self, tone, key):
         self.isKeyPress = False
         self.key = key
@@ -25,14 +24,12 @@ class PianoKey:
         self.stream.write('midi', fp=key + ".mid")
         # self.player = pygame.mixer.music.load(key + ".mid");
 
-
     def KeyPress(self):
         print('{0} pressed'.format(self.key))
         self.player = pygame.mixer.music.load(self.key + ".mid")
         self.player = pygame.mixer.music.play()
         # self.player.play();
         return
-
 
     def KeyRelease(self):
         print('{0} release'.format(self.key))
@@ -42,7 +39,7 @@ class PianoKey:
         return
 
 
-PianoKey_Setting = [['C', "'1'"], 
+PianoKey_Setting = [['C', "'1'"],
                     ['D', "'2'"],
                     ['E', "'3'"],
                     ['F', "'4'"],
@@ -80,5 +77,3 @@ if __name__ == "__main__":
     with pynput.keyboard.Listener(on_press=on_press_fun, on_release=
     on_release_fun) as listener:
         listener.join()
-        
-    
